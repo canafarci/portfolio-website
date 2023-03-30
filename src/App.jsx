7e41494c6d51b9fa0635d8bdc84f88f819f68e72
canafarci <ismetberke@gmail.com>
2023-03-30 22:19:11 +0300

    created empty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
