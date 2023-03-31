557fbc7f0d61499a74cc5bd9835fa4f88d958139
canafarci <ismetberke@gmail.com>
2023-03-31 16:43:19 +0300

    header mock finish

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import pfpic from "../../assets/pf-image.jpg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header__row">
+      <div className="header__container ">
+        <h5>Hello, my name is</h5>
+        <h1>Berke Cakir</h1>
+        <h3 className="text-light">Developer // Designer</h3>
+        <CTA />
+        <HeaderSocials />
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+      <div className="pfimage">
+        <img src={pfpic} alt="pfimage" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaLinkedin, FaGithub, FaBehanceSquare } from "react-icons/fa";
+import "./header.css";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://github.com/canafarci" target="_blank">
+        <FaGithub size={40} className="icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/berkecakir/" target="_blank">
+        <FaLinkedin size={40} className="icon" />
+      </a>
+      <a href="https://www.behance.net/berkecakir" target="_blank">
+        <FaBehanceSquare size={40} className="icon" />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
