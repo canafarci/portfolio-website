f9c94ad68102bb347fcd1f1cb4d996d479cda7df
canafarci <ismetberke@gmail.com>
2023-04-05 15:16:25 +0300

    portfolio start with cards

---
+++ src/components/portfolio/ContentData.jsx
@@ -0,0 +1,12 @@
+import kemerburgaz from "./arch-images/kemerburgaz-1.jpg";
+
+// Content data for the portfolio
+export const contentData = [
+  {
+    category: "architectural_visualizations",
+    title: "Modern House Design",
+    description: "A stunning modern house design visualization.",
+    image: kemerburgaz,
+    details: "https://example.com/modern-house-details",
+  },
+];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,159 @@
-import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import "./portfolio.css";
+import { contentData } from "./ContentData";
 
-const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`horizontal-tabpanel-${index}`}
+      aria-labelledby={`horizontal-tab-${index}`}
+      className="tabpanel_container"
+      {...other}
+    >
+      {value === index && <Box className="tab_right">{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
-export default Portfolio;
+function a11yProps(index) {
+  return {
+    id: `horizontal-tab-${index}`,
+    "aria-controls": `horizontal-tabpanel-${index}`,
+  };
+}
+
+export default function Portfolio() {
+  const [value, setValue] = React.useState(0);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleBackClick = () => {
+    setSelectedItem(null);
+  };
+
+  const tabs = [
+    "All",
+    "Mobile Games",
+    "PC Games",
+    "Multiplayer Games",
+    "Architectural Visualizations",
+    "Mobile Apps",
+    "Architectural Designs",
+    "Other Design Work",
+    "Other Development Work",
+  ];
+  const filteredContentData = contentData.filter(
+    (item) =>
+      value === 0 || // Show all items for the "All" tab
+      item.category === // Show items with matching category
+        tabs[value].toLowerCase().replace(/ /g, "_") // Convert tab label to category name
+  );
+
+  return (
+    <Box
+      className="tab_container"
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+      }}
+    >
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="Portfolio Tabs"
+        sx={{
+          width: "100%",
+          borderBottom: "1px solid black",
+        }}
+        variant="scrollable"
+        scrollButtons="auto"
+      >
+        {tabs.map((tab, index) => (
+          <Tab key={index} label={tab} {...a11yProps(index)} />
+        ))}
+      </Tabs>
+      {selectedItem ? (
+        <div className="portfolio_item_container">
+          <button className="back_button" onClick={handleBackClick}>
+            Back
+          </button>
+          <div className="portfolio_item_details">
+            <img
+              src={selectedItem.image}
+              alt={selectedItem.title}
+              className="portfolio_item_image"
+            />
+            <div className="portfolio_item_description">
+              <Typography variant="h4" className="portfolio_item_title">
+                {selectedItem.title}
+              </Typography>
+              <Typography variant="body 2" className="portfolio_item_text">
+                {selectedItem.description}
+              </Typography>
+              <a
+                href={selectedItem.details}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="portfolio_item_link"
+              >
+                View Details
+              </a>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="portfolio_cards_container">
+          {filteredContentData.map((item, index) => (
+            <div
+              key={index}
+              className="portfolio_card"
+              onClick={() => handleItemClick(item)}
+            >
+              <img
+                src={item.image}
+                alt={item.title}
+                className="portfolio_card_image"
+              />
+              <div className="portfolio_card_text">
+                <Typography variant="h5" className="portfolio_card_title">
+                  {item.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  className="portfolio_card_description"
+                >
+                  {item.description}
+                </Typography>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </Box>
+  );
+}
