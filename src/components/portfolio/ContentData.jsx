64c9bed91c6868bc26453ddd2507257d38dbb4fa
canafarci <ismetberke@gmail.com>
2023-05-16 00:35:32 +0300

    changed data structrue of the card contents
    revised dialog content

--- src/components/portfolio/ContentData.jsx
+++ src/components/portfolio/ContentData.jsx
@@ -2,32 +2,41 @@ export const contentData = [
   {
     category: "architectural_visualizations",
     title: "Kemerburgaz Villas",
-    date: "September 2020",
+    date: "  September 2020",
     location: "Kemerburgaz // İstanbul",
-    description: "Housing project in  Kemerburgaz/İstanbul",
-    detailed_description: "",
+    description: "Housing project in Kemerburgaz/İstanbul",
+    detailed_description:
+      "Façade, landscape and interior design work with 3D renders",
+    skills: "  3DS Max, Corona Renderer, Substance Painter, Photoshop",
+    job_type: "  Freelance Project",
     image:
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/140740152287837.631b2f94e7994.jpg",
     images: [
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/140740152287837.631b2f94e7994.jpg",
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/e56a9f152287837.631b2f94e9508.jpg",
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/dd025d152287837.631b2f94e8b7c.jpg",
-      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/69844f152287837.631b2f94e5e0f.jpg",
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/291b80152287837.631b2f94e67a1.jpg",
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/ca7858152287837.631b2f94e70b1.jpg",
       "https://mir-s3-cdn-cf.behance.net/project_modules/1400/860819152287837.631b2f94e82d2.jpg",
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/69844f152287837.631b2f94e5e0f.jpg",
     ],
     videoUrl: null,
     webGLUrl: null,
+    github_link: null,
+    behance_link:
+      "https://www.behance.net/gallery/152287837/Kemerburgaz-Villas",
   },
   {
     category: "pc_games",
     title: "Drunken Dungeon",
-    date: "July 2022",
+    date: "  July 2022",
     location: null,
     description: "An entry to the GMTK Game Jam 2022",
     detailed_description:
       "Traverse trough the dungeons with your un-trusty dice of elemental power! \nYou can only use 3 of your 4 powers, and the powers you can use and the buttons for those powers change depending on the roll of the dice.Defeat all of the enemies in the level to advance to the next level. \nControl using WASD keys, spacebar and mouse clicks, ranged actions aimed at cursor. \nRed - Ranged Fireball  \nBlue - Area Ice Attack \nBrown - Earth Shield \nWhite - Air Teleport",
+    skills: "  Unity 3D, C#",
+    role: "  Programming, Gameplay Design, Level Design",
+    job_type: "  Passion Project, completed with the Flowtart Studios team",
     image: "https://img.itch.zone/aW1nLzk1NDk3NDkucG5n/315x250%23c/iCFxeA.png",
     images: [
       "https://img.itch.zone/aW1hZ2UvMTYyMTE2OS85NTA3OTMwLnBuZw==/original/qEci2l.png",
@@ -37,6 +46,7 @@ export const contentData = [
     webGLUrl: "https://itch.io/embed-upload/7921006?color=ededed", // Add this only if WebGL exists
     webgl_width: "850",
     webgl_height: "580",
+    github_link: "https://github.com/canafarci/DrunkenWizard",
+    behance_link: null,
   },
-  // other items...
 ];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,9 +5,11 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
-import "./portfolio.css";
 import { contentData } from "./ContentData";
 import PortfolioCards from "./PortfolioCards";
+import { FaLinkedin, FaGithub, FaBehanceSquare } from "react-icons/fa";
+import "../../index.css";
+import "./portfolio.css";
 
 export default function Portfolio() {
   const [value, setValue] = React.useState(0);
@@ -76,7 +78,48 @@ export default function Portfolio() {
           {/* Render the content of the selected item in the dialog */}
           {selectedItem && (
             <>
-              <h2>{selectedItem.title}</h2>
+              <div className="socials">
+                <h2>{selectedItem.title}</h2>
+                {selectedItem.github_link && (
+                  <a
+                    href={selectedItem.github_link}
+                    target="_blank"
+                    className="button"
+                  >
+                    <FaGithub size={40} className="icon" />
+                    <h4> Source Code</h4>
+                  </a>
+                )}
+                {selectedItem.behance_link && (
+                  <a
+                    href={selectedItem.behance_link}
+                    target="_blank"
+                    className="button"
+                  >
+                    <FaBehanceSquare size={40} className="icon" />
+                    <h4> Behance</h4>
+                  </a>
+                )}
+              </div>
+              <div className="row">
+                <h4>Date: </h4>
+                <p>{selectedItem.date}</p>
+              </div>
+              <div className="row">
+                <h4>Skills: </h4>
+                <p>{selectedItem.skills}</p>
+              </div>
+              <div className="row">
+                <h4>Job Type: </h4>
+                <p>{selectedItem.job_type}</p>
+              </div>
+              {selectedItem.role && (
+                <div className="row">
+                  <h4>Role: </h4>
+                  <p>{selectedItem.role}</p>
+                </div>
+              )}
+
               <p>{selectedItem.description}</p>
               <p>{selectedItem.detailed_description}</p>
               {selectedItem.webGLUrl && (
